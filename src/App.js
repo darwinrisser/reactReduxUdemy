b98a6e91635d85ef242ee69ed073d4e68a2ccea0
Darwin <risser.darwin@gmail.com>
2018-12-12 14:23:11 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -10,11 +10,19 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-      {name: 'maximo', age: 28},
+      {name: newName, age: 28},
       {name: 'manu', age: 32}
+    ]})
+  }
+
+  nameChangedHandler=(event)=>{
+    this.setState({
+      persons: [
+      {name: 'MAAAX', age: 28},
+      {name: event.target.value, age: 29}      
     ]})
   }
 
@@ -23,9 +31,18 @@ class App extends Component {
       <div className="App">
         <h1>Sup bitches</h1>
         <p>Dafuq</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}><h1>Hobbies: gaming</h1></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={()=> this.switchNameHandler('Maximo!!!!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}>
+          <h1>Hobbies: gaming</h1>
+        </Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Maximo')}
+          changed={this.nameChangedHandler} />
       </div>
 
     );
